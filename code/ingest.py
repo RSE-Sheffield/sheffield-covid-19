a3f046a231d40966e53f734f922b44444a28bf32
David <david.r.jones@sheffield.ac.uk>
2020-10-07 16:59:55 +0100

    throw in some html5lib and xpath

--- code/ingest.py
+++ code/ingest.py
@@ -10,12 +10,17 @@ COVID-19 dashboard
 
 # https://requests.readthedocs.io/en/master/
 import requests
+# https://pypi.org/project/html5lib/
+import html5lib
 
 URL="https://www.sheffield.ac.uk/autumn-term-2020/covid-19-statistics/"
 
 def main():
     response = requests.get(URL)
-    print(response.text)
+    dom = html5lib.parse(response.text,
+        namespaceHTMLElements=False)
+    things = dom.findall(".//{}td")
+    print(things)
 
 if __name__ == "__main__":
     main()
