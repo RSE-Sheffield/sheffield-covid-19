c9782b7a2a1617d8fd3f332b6d83c0b037b46df0
David <david.r.jones@sheffield.ac.uk>
2020-10-08 11:31:51 +0100

    Find children of rows

--- code/ingest.py
+++ code/ingest.py
@@ -8,8 +8,12 @@ The University of Sheffield's
 COVID-19 dashboard
 """
 
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+import xml.etree
+
 # https://requests.readthedocs.io/en/master/
 import requests
+
 # https://pypi.org/project/html5lib/
 import html5lib
 
@@ -17,10 +21,14 @@ URL="https://www.sheffield.ac.uk/autumn-term-2020/covid-19-statistics/"
 
 def main():
     response = requests.get(URL)
-    dom = html5lib.parse(response.text,
-        namespaceHTMLElements=False)
-    things = dom.findall(".//{}td")
-    print(things)
+    dom = html5lib.parse(response.text, namespaceHTMLElements=False)
+    things = dom.findall(".//tr")
+    for thing in things:
+        for el in thing:
+            content = xml.etree.ElementTree.tostring(el, encoding="unicode")
+            content = content.strip()
+            print(content)
+
 
 if __name__ == "__main__":
     main()
